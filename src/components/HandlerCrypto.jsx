597130ba4546298ba660fab4f18d61cf58a8bbf1
Johang31m <johansegom08@gmail.com>
2023-01-16 14:43:55 -0500

    Esta completo

--- src/components/HandlerCrypto.jsx
+++ src/components/HandlerCrypto.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Component } from "react";
+import { Transition } from "react-transition-group";
 import './HandlerCrypto.css'
 
 let url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=7a2265da-55d4-4fce-9ee9-9721b6e17845';
@@ -8,35 +9,40 @@ let url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?C
 class CryptoBody extends Component {
   state = {
     data: null,
-    name: '',
-    symbol: '',
-    price: 0,
-
   };
 
   ObtenerData = (e) => {
     // setInterval(() => {
-      fetch(url)
-        .then(response => response.json())
-        .then(data => {
-          this.setState({ data })
-          console.log(data.data[0])
-        })
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ data })
+        console.log(data)
+      })
     // }, 10000)
 
   }
   render() {
+    const container = [];
     if (this.state.data === null) {
-      return <p onClick={this.ObtenerData} className="ContenedorData" style={{ color: 'white', fontSize: '40px' }}>Cargando...</p>;
+      return <p onClick={-this.ObtenerData} className="ContenedorData" style={{ color: 'white', fontSize: '40px' }}>Cargando...</p>;
+    }
+    else{
+      for (let i in this.state.data.data) {
+        if (this.state.data.data[i].symbol == 'BTC') {
+          console.log(this.state.data.data[i])
+          container.push(<div key={this.state.data.data[i].id} className="ContenedorData" >
+            <div className="DataCrypto">Simbolo: {this.state.data.data[i].symbol}  </div>
+            <div className="DataCrypto">Nombre: {this.state.data.data[i].name}</div>
+            <div className="DataCrypto">Precio: {(this.state.data.data[i].quote.USD.price).toFixed(3)}</div>
+            <div className="DataCrypto">Rank: {this.state.data.data[i].cmc_rank} </div>
+          </div>)
+        }
+      }
     }
     return (
       <div className="ContenedorData">
-        <div className="ContenedorData" >
-          <div className="DataCrypto">Simbolo: {this.state.data.data[0].symbol}  </div>
-          <div className="DataCrypto">Nombre: {this.state.data.data[0].name}</div>
-          <div className="DataCrypto">Precio: {(this.state.data.data[0].quote.USD.price).toFixed(3)}</div>
-          <div className="DataCrypto">Rank: {this.state.data.data[0].cmc_rank} </div>
-        </div>
+        {container}
       </div>
     );
   }
