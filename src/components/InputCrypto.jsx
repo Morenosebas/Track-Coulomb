8ba08a5914c58025a6aac4557336f4b8c3ab0795
Johang31m <johansegom08@gmail.com>
2023-02-13 00:01:17 -0500

    search results error al tener 3 caracteres

--- src/components/InputCrypto.jsx
+++ src/components/InputCrypto.jsx
@@ -21,47 +21,51 @@ class InputCrypto extends Component {
     HandleBusquedad = (e) => {
         // eslint-disable-next-line react/no-direct-mutation-state
         this.setState({ dataSearchHandle: e.target.value })
-        if (this.state.dataSearchHandle.length > 1) {
-            if (this.state.cache[this.state.dataSearchHandle]) {
+        const dataSearchHandle = this.state.dataSearchHandle;
+        setTimeout(() => {
+            if (dataSearchHandle.length > 2) {
                 this.SetShowResult(true)
-                console.log('data cache', this.state.cache)
-                // Si el resultado ya está en caché, actualiza el estado del componente
-                this.setState({
-                    dataSearch: this.state.cache[this.state.dataSearchHandle],
-                });
-            } else {
-                console.log('data update online')
-                let nameAux = new RegExp('^' + this.state.dataSearchHandle + '\\w+', 'i');
-                let symbolAux = new RegExp('^' + this.state.dataSearchHandle + '$', 'i');
-                fetch(url)
-                    .then(response => response.json())
-                    .then(data => {
-                        const dataArray = data.data.reduce((acc, curr) => {
-                            const dataName = curr.name;
-                            const dataSlug = curr.slug;
-                            const dataSymbol = curr.symbol;
-                            if ((symbolAux.test(dataName)) || (nameAux.test(dataSlug)) || (symbolAux.test(dataSymbol))) {
-                                acc.unshift(dataName);
-                            }
-                            return acc;
-                        }, []);
-                        // Almacena el resultado de la búsqueda en caché
-                        const cache = {
-                            ...this.state.cache,
-                            [this.state.dataSearchHandle]: dataArray,
-                        };
-                        this.setState({
-                            dataSearch: dataArray,
-                            cache: cache,
-                        });
-                        console.log('data data', this.state.cache)
-                        this.SetShowResult(true)
+                if (this.state.cache[dataSearchHandle]) {
+                    console.log('data cache', this.state.cache)
+                    // Si el resultado ya está en caché, actualiza el estado del componente
+                    this.setState({
+                        dataSearch: this.state.cache[dataSearchHandle],
                     });
+                } else {
+                    console.log('data update online')
+                    let nameAux = new RegExp(dataSearchHandle, 'i');
+                    // let symbolAux = new RegExp('^' + dataSearchHandle + '\\D','i');
+                    fetch(url)
+                        .then(response => response.json())
+                        .then(data => {
+                            const dataArray = data.data.reduce((acc, curr) => {
+                                const dataName = curr.name;
+                                const dataSlug = curr.slug;
+                                const dataSymbol = curr.symbol;
+                                if ((nameAux.test(dataName)) || (nameAux.test(dataSlug)) || (nameAux.test(dataSymbol))) {
+                                    acc.unshift(dataName);
+                                }
+                                return acc;
+                            }, []);
+                            // Almacena el resultado de la búsqueda en caché
+                            const cache = {
+                                ...this.state.cache,
+                                [dataSearchHandle]: dataArray,
+                            };
+                            this.setState({
+                                dataSearch: dataArray,
+                                cache: cache,
+                            });
+                            console.log('data data', this.state.dataSearch)
+                            this.SetShowResult(true)
+                        });
+                }
+            } else {
+                this.setState({ dataSearch: [] })
+                this.SetShowResult(false)
             }
-        } else {
-            this.setState({ dataSearch: []})
-            this.SetShowResult(false)
-        }
+        }, 500)
+
     }
 
 
@@ -71,7 +75,6 @@ class InputCrypto extends Component {
         this.SetShowResult(false);
     }
     KeyDownHandler = (e) => {
-
         if (e.key === 'ArrowDown') {
 
             try {
@@ -91,14 +94,18 @@ class InputCrypto extends Component {
 
         } else if (e.key === 'Enter') {
             try {
-                e.target.value = this.state.dataSearch[this.state.currentIndex]
+                if (this.state.currentIndex != -1) {
+                    e.target.value = this.state.dataSearch[this.state.currentIndex]
+                }
                 console.log('desde el enter')
             } catch (error) { console.error(error) }
         }
     }
     componentDidMount() {
         console.log('ONLINE')
+        this.setState({ showResult: false })
     }
+
 
     render() {
         var div = [];
@@ -111,7 +118,7 @@ class InputCrypto extends Component {
             <>
                 <form id="MyForm" className="Form">
                     <input autoComplete="off" type="text" onChange={this.HandleBusquedad} name="Busquedad" id="Busquedad" placeholder="Escribe el nombre o la abreviatura: "
-                        onFocus={() => this.SetShowResult(true)} onBlur={() => setTimeout(() => this.SetShowResult(false), 100)}
+                        onFocus={() => this.SetShowResult(true)} onBlur={() => setTimeout(() => this.SetShowResult(false), 200)}
                         onKeyDown={(e) => this.KeyDownHandler(e)}
                     />
                     <button onClickCapture={this.props.myOnClick} onClick={this.BuscarData} type="submit" id={'Buscar'} >Buscar</button>
